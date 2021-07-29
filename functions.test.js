7848ac165f05a19e027c71f0c7ca70338900f985
Devish Baidawar Chhetri <devish@noveltytechnology.com>
2021-07-29 15:59:09 +0545

    describe() order of execution

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,30 @@
 const functions = require('./functions');
 
+// beforeEach(() => initDatabase());
+// afterEach(() => closeDatabase());
+
+// beforeAll(() => initDatabase());
+// afterAll(() => closeDatabase());
+
+// const initDatabase = () => console.log('Database Initialized');
+// const closeDatabase = () => console.log('Database Closed');
+
+const nameCheck = () => console.log('Checking name...');
+
+describe('Checking Names', () => {
+  beforeEach(() => nameCheck());
+
+  test('User is Devish', () => {
+    const user = 'Devish';
+    expect(user).toBe('Devish');
+  });
+
+  test('User is Chhetri', () => {
+    const user = 'Chhetri';
+    expect(user).toBe('Chhetri');
+  });
+});
+
 // toBe
 test('Adds 2 + 2 to equal to 4', () => {
   expect(functions.add(2,2)).toBe(4);
