d25980df04728094d1f3880a417b28e62d8493e5
Devish Baidawar Chhetri <devish@noveltytechnology.com>
2021-07-29 14:11:41 +0545

    async data test

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
@@ -6,7 +8,15 @@ const functions = {
     const user = { firstName: 'Devish'};
     user['lastName'] = 'Chhetri';
     return user;
-  }
+  },
+  fetchUser: () => 
+    axios
+      .get('https://jsonplaceholder.typicode.com/users/1')
+      .then(response => response.data)
+      .catch(error => error.response.data)
 }
+
+
+
 
 module.exports = functions;
